4072e7ea4db4942482a21a368d4c4b4fd3194995
LOOPER <iamloooper@gmail.com>
2022-04-10 20:52:32 +0500

    Updated to v0.3 with various improvements
    Signed-off-by: LOOPER <iamloooper@gmail.com>

--- service.sh
+++ service.sh
@@ -1,13 +1,23 @@
 #!/system/bin/sh
 # XLoad™ | Service Script
 
-wait_until_boot_complete() {
+wait_until_login() {
+  # In case of /data encryption is disabled
   while [[ "$(getprop sys.boot_completed)" != "1" ]]; do
     sleep 2
   done
+
+  # We don't have the permission to rw "/storage/emulated/0" before the user unlocks the screen
+  local test_file="/storage/emulated/0/Android/.PERMISSION_TEST"
+  true > "$test_file"
+  while [[ ! -f "$test_file" ]]; do
+    true > "$test_file"
+    sleep 1
+  done
+  rm -rf "$test_file"
 }
 
-wait_until_boot_complete
+wait_until_login
 
 # Initialize XLoad™
-xload --init
+xload --initialize
