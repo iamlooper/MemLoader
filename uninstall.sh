f333c0e9bd6ccbf821f01aa5c65fa8394cea19a7
iamlooper <iamloooper@gmail.com>
2022-08-12 10:13:45 +0000

    Updated to v0.6
    - Improved menu
    - Added auto locking on app launch feature
    - Added new unloading logic
    - Misc. bug fixes
    
    Signed-off-by: iamlooper <iamloooper@gmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -11,9 +11,9 @@ rm -rf "/storage/emulated/0/XLoad"
       rm -rf "$LINE"
       while true; do
         LINE="$(dirname "$LINE")"
-        [[ "$(ls -A $LINE 2>/dev/null)" ]] && break 1 || rm -rf "$LINE"
+        [[ "$(ls -A "$LINE" 2>/dev/null)" ]] && break 1 || rm -rf "$LINE"
       done
     fi
-  done < "$INFO"
+  done< "$INFO"
   rm -rf "$INFO"
 }
